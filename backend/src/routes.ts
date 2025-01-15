de07652e8c370a53cee58cf53ac91e9e58346dfd
Bognar Junior <bognar_junior@yahoo.com.br>
2025-01-15 16:00:57 -0300

    Create route
    Create route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,5 +6,7 @@ import {
 } from 'fastify';
 
 export async function routes(fastify: FastifyInstance, opts: FastifyPluginOptions) {
-  
+  fastify.get('/teste', async (request: FastifyRequest, reply: FastifyReply) => {
+    reply.send({ hello: 'world' });
+  });
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors';
 import dotenv from 'dotenv';
+import { routes } from './routes';
 
 const app = fastify({ logger: true });
 dotenv.config();
@@ -9,6 +10,17 @@ app.setErrorHandler((error, request, reply) => {
   reply.code(400).send({ error: error.message });
 });
 
-const start = async () => {};
+const start = async () => {
+  app.register(cors);
+  app.register(routes);
+  
+  try {
+    await app.listen({ port: 3333, host: '0.0.0.0' });
+    console.log("Servidor rodando na porta 3333");
+  } catch (error) {
+    console.log(error);
+  }
+  
+};
 
 start();
