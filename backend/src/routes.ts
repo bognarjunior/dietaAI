8aabcb6c0dfa04bf7ce4c7ba7399797024c172c0
Bognar Junior <bognar_junior@yahoo.com.br>
2025-01-15 16:51:30 -0300

    Revert "Merge branch 'main' of https://github.com/bognarjunior/dietaAI"
    This reverts commit f6346ba4251d9924b6a659628dfba58c9d4770a5, reversing
    changes made to 0e01c2823a71f63af11ee0081245f8a40fd9f4b8.

---
+++ backend/src/routes.ts
@@ -0,0 +1,18 @@
+import {
+  FastifyInstance, 
+  FastifyReply, 
+  FastifyRequest, 
+  FastifyPluginOptions
+} from 'fastify';
+
+import { CreateNutritionController } from './controllers/CreateNutritionController';
+
+export async function routes(fastify: FastifyInstance, opts: FastifyPluginOptions) {
+  fastify.get('/teste', async (request: FastifyRequest, reply: FastifyReply) => {
+    reply.send({ hello: 'world' });
+  });
+
+  fastify.get('/create', async (request: FastifyRequest, reply: FastifyReply) => {
+    return new CreateNutritionController().handle(request, reply);
+  });
+}
