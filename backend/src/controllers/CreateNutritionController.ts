0e01c2823a71f63af11ee0081245f8a40fd9f4b8
Bognar Junior <bognar_junior@yahoo.com.br>
2025-01-15 16:29:24 -0300

    Create .gitignore

--- backend/src/controllers/CreateNutritionController.ts
+++ backend/src/controllers/CreateNutritionController.ts
@@ -3,7 +3,7 @@ import { FastifyRequest, FastifyReply } from "fastify";
 class CreateNutritionController {
   async handle(request: FastifyRequest, reply: FastifyReply) {
     //const { name, description, kcal, protein, carbs, fat } = request.body;
-    reply.send({ message: 'Sua Dieta foi criada com sucesso' });
+    reply.send({ message: 'Sua Dieta foi criada com sucesso!' });
   }
 }
 
