def128afc95f86b24ac6cd486e613362cfbe25b1
Bognar Junior <bognar_junior@yahoo.com.br>
2025-01-22 15:20:45 -0300

    Update nutrition

--- mobile/src/app/nutrition/index.tsx
+++ mobile/src/app/nutrition/index.tsx
@@ -1,19 +1,21 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, ScrollView, Share } from 'react-native';
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 
-
 import { Data, useData } from '@/store/data';
 import api from '@/app/services/api';
+
 import { styles } from './style';
+import { Link, router } from 'expo-router';
+import { Feather, Ionicons } from '@expo/vector-icons';
 
 interface ResponseData {
   data: Data
 }
 
 export default function Nutrition() {
-  const user =  useData((state) => state.data)
-  console.log('user', user);
+  const user =  useData((state) => state.data);
+
   const {data, isFetching, error} = useQuery({
     queryKey: ['nutrition'],
     queryFn: async () => {
@@ -22,7 +24,7 @@ export default function Nutrition() {
         if(!user) {
           throw new Error('Falha ao carregar os dados');
         }
-        console.log('Entra aqui')
+        
         const response = await api.post<ResponseData>('/create', {
           name: user.name,
           weight: user.weight,
@@ -32,14 +34,31 @@ export default function Nutrition() {
           level: user.level,
           objective: user.objective,
         });
-        console.log('response',response.data);
-        return response.data;
+        return response.data.data;
 
       } catch (error) {
         console.log('Erro',error);
       }
     }
   });
+
+  async function handleShare() {
+    try {
+      if(data && Object.keys(data).length === 0) return; 
+      const supplements = `${data?.suplementos.map( item => ` ${item}`)}`
+
+      const foods = `${data?.refeicoes.map( item => `\n- Nome: ${item.nome}\n- Horário: ${item.horario}\n- Alimentos: ${item.alimentos.map( alimento => ` ${alimento}` )}`)}`
+
+      const message = `Dieta: ${data?.nome} - Objetivo: ${data?.objetivo}\n\n${foods}\n\n- Dica Suplemento: ${supplements}`
+
+      await Share.share({
+        message: message
+      })
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   if(isFetching) {
     return (
@@ -50,11 +69,74 @@ export default function Nutrition() {
     )
   }
 
-  console.log('data', data);
+  if(error) {
+    return (
+      <View style={styles.loading}>
+        <Text style={styles.loadingText}>Falha ao carregar sua dieta!</Text>
+        <Link href={'/step'}>
+          <Text style={styles.loadingText}>Consultando AI...</Text>
+        </Link>
+      </View>
+    )
+  }
  
   return (
-    <View>
-      <Text>Nutrition</Text>
+    <View style={styles.container}>
+      <View style={styles.containerHeader}>
+        <View style={styles.contentHeader}>
+          <Text style={styles.titleHeader}>Minha dieta!</Text>
+          <Pressable style={styles.buttonHeader} onPress={handleShare}>
+            <Text style={styles.buttonTextHeader}>Compartilhar</Text>
+          </Pressable>
+        </View>
+      </View>
+
+      <View 
+        style={styles.list}
+      >
+        {data && Object.keys(data).length > 0 && (
+          <>
+            <Text style={styles.name}>Nome: {data.nome}</Text>
+            <Text style={styles.objective}>Objetivo: {data.objetivo}</Text>
+            <Text style={styles.label}>Refeições: </Text>
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+              <View style={styles.foods}>
+                {data.refeicoes.map((refeicao) => (
+                  <View key={refeicao.nome} style={styles.food}>
+                    <View style={styles.foodHeader} >
+                      <Text style={styles.foodTitle}>{refeicao.nome}</Text>
+                      <Ionicons name="restaurant" size={16} color="black" />
+                    </View>
+
+                    <View style={styles.foodContent}>
+                      <Feather name="clock" size={16} color="black" />
+                      <Text style={styles.foodTime}>Horário: {refeicao.horario}</Text>
+                    </View>
+
+                    <Text style={styles.foodLabel}>Alimentos:</Text>
+                    {refeicao.alimentos.map((alimento) => (
+                      <Text key={alimento}>{alimento}</Text>
+                    ))}
+                  </View>
+                ))}
+              </View>
+              <View style={styles.supplement}>
+                <Text style={styles.foodTitle}>Dica de suplementos:</Text>
+                {data.suplementos.map((suplemento) => (
+                  <Text key={suplemento}>{suplemento}</Text>
+                ))}
+              </View>
+
+              <Pressable style={styles.button} onPress={ () => router.replace("/") }>
+                <Text style={styles.buttonText}>
+                  Gerar nova dieta
+                </Text>
+              </Pressable>
+            </ScrollView>
+          </>
+        )}
+      </View>
     </View>
   )
 }
--- mobile/src/app/nutrition/style.ts
+++ mobile/src/app/nutrition/style.ts
@@ -1,9 +1,43 @@
 import { StyleSheet } from "react-native";
 import { theme } from "@/theme";
+import { object } from "zod";
 
 const { colors } = theme;
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: colors.background
+  },
+  containerHeader: {
+    backgroundColor: colors.white,
+    borderBottomLeftRadius: 16,
+    borderBottomRightRadius: 16,
+    paddingBottom: 20,
+    paddingTop: 60,
+    marginBottom: 20
+  },
+  contentHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16
+  },
+  titleHeader: {
+    fontSize: 28,
+    color: colors.background,
+    fontWeight: 'bold',
+  },
+  buttonHeader: {
+    backgroundColor: colors.blue,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+    borderRadius: 6
+  },
+  buttonTextHeader: {
+    color: colors.white,
+    fontWeight: '500'
+  },
   loading: {
     flex: 1,
     justifyContent: 'center',
@@ -14,5 +48,84 @@ export const styles = StyleSheet.create({
     fontSize: 18,
     color: colors.white,
     marginBottom: 8
+  },
+  list: {
+    flex: 1,
+    paddingHorizontal: 16
+  },
+  name: {
+    fontSize: 20,
+    color: colors.white,
+    fontWeight: 'bold'
+  },
+  objective: {
+    fontSize: 16,
+    color: colors.white,
+    marginBottom: 24,
+  },
+  label: {
+    fontSize: 16,
+    color: colors.white,
+    fontWeight: 'bold',
+    marginBottom: 8
+  },
+  foods: {
+    backgroundColor: colors.white,
+    borderRadius: 8,
+    padding: 16,
+    gap: 12
+  },
+  food: {
+    backgroundColor: 'rgba(208, 208, 208, 0.4)',
+    padding: 12,
+    borderRadius: 8
+  },
+  foodHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 8
+  },
+  foodContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8
+  },
+  foodTitle: {
+    fontSize: 16,
+    color: colors.black,
+    fontWeight: 'bold'
+  },
+  foodLabel: {
+    fontSize: 16,
+    marginTop: 14,
+    marginBottom: 8,
+    color: colors.black,
+    fontWeight: 'bold'
+  },
+  supplements: {
+    backgroundColor: colors.white,
+    borderRadius: 8,
+    padding: 16,
+    gap: 12
+  },
+  supplement: {
+    backgroundColor: colors.white,
+    marginTop: 14,
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 20
+  },
+  button: {
+    backgroundColor: colors.blue,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+    borderRadius: 8,
+    marginBottom: 30
+  },
+  buttonText: {
+    color: colors.white,
+    fontWeight: '500'
   }
 });
